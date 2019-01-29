b8f48677494226ede259bb9250f9a43f19728b75
Sangam <36110390+sangaml@users.noreply.github.com>
2019-01-29 21:02:58 +0530

    Update nifi.sh

--- nifi.sh
+++ nifi.sh
@@ -3,8 +3,8 @@
 wget http://mirrors.estointernet.in/apache/nifi/1.8.0/nifi-1.8.0-bin.tar.gz
 tar -xvf nifi-1.8.0-bin.tar.gz
 yum install java -y
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-ibm-1.8.0.5.25-1jpp.1.el7.x86_64/jre
 cd nifi-1.8.0
 bin/nifi.sh install
-bin/nifi.sh run
 bin/nifi.sh start
 iptables -F
