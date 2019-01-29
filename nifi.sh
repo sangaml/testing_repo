133bd3c9540324c77503ae3fc9a3b4d90038881a
Sangam <36110390+sangaml@users.noreply.github.com>
2019-01-29 20:49:18 +0530

    Update nifi.sh

--- nifi.sh
+++ nifi.sh
@@ -3,6 +3,7 @@
 wget http://mirrors.estointernet.in/apache/nifi/1.8.0/nifi-1.8.0-bin.tar.gz
 tar -xvf nifi-1.8.0-bin.tar.gz
 yum install java -y
+cd nifi-1.8.0
 bin/nifi.sh install
 bin/nifi.sh run
 bin/nifi.sh start
