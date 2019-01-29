2a3f3d8f9599e1bdb204821707e298e9daf45a87
Sangam <36110390+sangaml@users.noreply.github.com>
2019-01-29 18:55:45 +0530

    Create nifi.sh

---
+++ nifi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Downloading nifi package for redhat 7.4
+wget http://mirrors.estointernet.in/apache/nifi/1.8.0/nifi-1.8.0-bin.tar.gz
+tar -xvf nifi-1.8.0-bin.tar.gz
+yum install java -y
+bin/nifi.sh install
+bin/nifi.sh run
+bin/nifi.sh start
+iptables -F
