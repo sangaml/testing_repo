e4368d3f8f60b5c3f3d137271e2b7373a251fda8
Sangam <36110390+sangaml@users.noreply.github.com>
2019-08-25 18:19:56 +0530

    Update installdocker.sh

--- k8s/installdocker.sh
+++ k8s/installdocker.sh
@@ -6,3 +6,4 @@ apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install docker-ce -y
+swapoff -a
