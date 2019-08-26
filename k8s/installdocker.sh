e95bf1b42d706b9f5a1ac19075e5e0874feb07df
Sangam <36110390+sangaml@users.noreply.github.com>
2019-08-26 11:35:22 +0530

    Update installdocker.sh

--- k8s/installdocker.sh
+++ k8s/installdocker.sh
@@ -1,8 +1,23 @@
+#############Install Docker 17.03.2 ################
+
 #!/bin/bash
 apt-get update -y
 apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
-apt-get install docker-ce=17.03.2-ce.0-ubuntu-xenial -y
+apt-get install docker-ce=17.03.2~ce-0~ubuntu-xenial -y
 swapoff -a
+
+#####Install Kubadm ##########
+
+apt-get update && apt-get install -y apt-transport-https curl 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+
+
+
