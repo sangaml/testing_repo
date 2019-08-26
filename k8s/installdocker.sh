7ea3c62e65442d39146eb65f48651234c708b451
Sangam <36110390+sangaml@users.noreply.github.com>
2019-08-26 11:08:49 +0530

    Update installdocker.sh

--- k8s/installdocker.sh
+++ k8s/installdocker.sh
@@ -4,5 +4,5 @@ apt-get install apt-transport-https ca-certificates curl software-properties-com
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
-apt-get install docker-ce=17.03.2-ce.0-ubuntu-xenial
+apt-get install docker-ce=17.03.2-ce.0-ubuntu-xenial -y
 swapoff -a
